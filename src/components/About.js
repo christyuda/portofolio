5548f2b832ae7d686d0df37ab7132da1e2b63b3c
christyuda <yudacihuy09@gmail.com>
2023-07-02 02:01:33 +0700

    home, about

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,82 @@
 import React from "react";
 
+import CountUp from "react-countup";
+
+import { useInView } from "react-intersection-observer";
+
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../variants";
+
 const About = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
   return (
-    <div className="section" id="about">
-      About
-    </div>
+    <section className="section" id="about" ref={ref}>
+      <div className="container mx-auto">
+        <motion.div
+          variants={fadeIn("right", 0.3)}
+          inlist={"hidden"}
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className="flex flex-col gap-y-10 lg:flex-row lg:items-center
+         lg:gap-x-10 lg:gap-y-0 h-screen"
+        >
+          {/* {img} */}
+          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[540px] mix-blend-lighten bg-top "></div>
+          {/* {text} */}
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            inlist={"hidden"}
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            <h2 className="h2 text-accent">About Me</h2>
+            <h3 className="h3 mb-4">
+              Seorang Pekerja Lepas Front End developer dengan 2 tahun
+              pengalaman.
+            </h3>
+            <p className="mb-6">blabalbalblablbalblalbl</p>
+            <div className="flex gap-x-6 lg:gap-x-10 mb-12">
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={2} duration={3} /> : null}
+                </div>
+                <div className=" font-primary text-sm tracking-[2px]">
+                  Years of <br /> Experience
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={15} duration={3} /> : null}
+                  k+
+                </div>
+                <div className=" font-primary text-sm tracking-[2px]">
+                  Project <br /> Completed
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={12} duration={3} /> : null}
+                  k+
+                </div>
+                <div className=" font-primary text-sm tracking-[2px]">
+                  Satisfied <br /> Clients
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-x-8 items-center">
+              <button className="btn btn-lg">Contact Me</button>
+              <a href="" className="text-gradient btn-link">
+                My Portofolio
+              </a>
+            </div>
+          </motion.div>
+        </motion.div>
+      </div>
+    </section>
   );
 };
 
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
+
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
-  return <div className='section' id='work'>Work</div>;
+  return (
+    <section className="section" id="work">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row gap-x-10"></div>
+        <div>2</div>
+      </div>
+    </section>
+  );
 };
 
 export default Work;
