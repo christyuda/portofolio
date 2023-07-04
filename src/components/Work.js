9237e608de92a95ef85ac1941d899e060e8799a6
christyuda <yudacihuy8@gmail.com>
2023-07-04 08:59:28 +0700

    change

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,7 +3,7 @@ import React from "react";
 import Img1 from "../assets/portfolio-img1.png";
 import Img2 from "../assets/portfolio-img2.png";
 import Img3 from "../assets/portfolio-img3.png";
-
+ 
 const Work = () => {
   return (
     <section className="section" id="work">
